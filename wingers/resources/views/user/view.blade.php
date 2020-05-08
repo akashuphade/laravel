55391d061e641bfac48f25416f7bc765cdea17cb
Akash Uphade <akash.uphade@aventri.com>
2020-05-08 17:31:56 +0530

    added profile view

--- wingers/resources/views/user/view.blade.php
+++ wingers/resources/views/user/view.blade.php
@@ -10,8 +10,32 @@
         </div>
         <div class="col-md-6 m-4">
             <h1>{{ Auth::user()->name }}</h1>
-            <h3>
+            <h3>Software Developer</h3>
         </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-md-6 float-right">
+            <nav>
+                <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link active" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="true">About</a>
+                </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
+                    <form>
+                        <div class="form-group row">
+                            <label class="col-form-label col-md-4 text-right font-weight-bold">Name:</label>
+                            <label class="col-md-6 col-form-label">{{ $user->name }}</label>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-form-label col-md-4 text-right font-weight-bold">E-mail:</label>
+                            <label class="col-md-6 col-form-label">{{ $user->email }}</label>
+                        </div>
+                    </form>
+                </div>
+                        
+            </div>
     </div>
         
 </div>
